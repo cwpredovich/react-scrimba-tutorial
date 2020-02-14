8ff144239a042694178d8dac26a9175019539a4d
Christopher <cwpredovich@yahoo.com>
2020-02-14 12:20:01 -0700

    Moved the functional component called About to it's own .js file. Moved that into a new folder called Components.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import About from './Components/About';
 
-function About() {
-    return (
-        <div>
-            <h1>Rick Steves</h1>
-            <p>Rick Steves is an American travel writer, author, activist, and television personality.</p>
-            <h4>Recommended Travel Destinations in Italy</h4>
-            <ol>
-                <li>The Cinque Terre</li>
-                <li>Sorrento</li>
-                <li>The Amalfi Coast</li>
-            </ol>
-        </div>
-    )
-};
 
 ReactDOM.render(<About />, document.getElementById('root'));
 
