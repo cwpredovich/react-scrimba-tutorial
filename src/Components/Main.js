8fd0fb69ce547671b9127145cc13fe31f4c36f56
Christopher <cwpredovich@yahoo.com>
2020-02-15 19:27:42 -0700

    Completed section 17 of the tutorial, JSX to JavaScript and back.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,15 +2,30 @@ import React from "react";
 
 
 function Main() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay
+
+    if (hours < 12) {
+        timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon";
+    } else {
+        timeOfDay = "evening";
+    };
+
     return (
-        <form>
-            <ul>
-                <li><input type="checkbox"></input></li>
-                <li><input type="checkbox"></input></li>
-                <li><input type="checkbox"></input></li>
-                <li><input type="checkbox"></input></li>
-            </ul>
-        </form>
+        <div>
+            <p>Good {timeOfDay}!</p>
+            <form>
+                <ul className="list">
+                    <li><input type="checkbox"></input></li>
+                    <li><input type="checkbox"></input></li>
+                    <li><input type="checkbox"></input></li>
+                    <li><input type="checkbox"></input></li>
+                </ul>
+            </form>
+        </div>
     )
 }
 
